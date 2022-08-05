9163b8c7eb5e768ef7364217ed2791a627f4af88
[prasanna] <prasanna>
2022-08-05 10:12:50 +0530

    [prasanna]: stacks and binary trees

---
+++ Stacks/BinaryTrees.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    internal class BinaryTrees
+    {
+        public class Node
+        {
+            public int Data;
+            public Node Left;
+            public Node Right;
+            public void DisplayNode()
+            {
+                Console.Write(Data + " ");
+            }
+        }
+        public Node root;
+        public BinaryTrees()
+        {
+            root = null;
+        }
+        public void Add(int i)
+        {
+            Node newNode = new Node();
+            newNode.Data = i;
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.Data)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+
+                        else
+                        {
+                            current = current.Right;
+                            if (current == null)
+                            {
+                                parent.Right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Stacks/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //stack st = new stack();
+            /* while (true)
+             {
+                 Console.Clear();
+                 Console.WriteLine("\nStack MENU(size -- 10)");
+                 Console.WriteLine("1. Add an element");
+                 Console.WriteLine("2. See the Top element.");
+                 Console.WriteLine("3. Remove top element.");
+                 Console.WriteLine("4. Display stack elements.");
+                 Console.WriteLine("5. Exit");
+                 Console.Write("Select your choice: ");
+                 int choice = Convert.ToInt32(Console.ReadLine());
+                 switch (choice)
+                 {
+                     case 1:
+                         Console.WriteLine("Enter an Element : ");
+                         st.Push(Console.ReadLine());
+                         break;
+
+                     case 2:
+                         Console.WriteLine("Top element is: {0}", st.Peek());
+                         break;
+
+                     case 3:
+                         Console.WriteLine("Element removed: {0}", st.Pop());
+                         break;
+
+                     case 4:
+                         st.Display();
+                         break;
+
+                     case 5:
+                         System.Environment.Exit(1);
+                         break;
+                 }
+                 Console.ReadKey();
+             }*/
+
+            //BinarySearchTrees---------------------------------------------------------------------------
+            BinaryTrees nums = new BinaryTrees();
+            nums.Add(50);
+            nums.Add(17);
+            nums.Add(23);
+            nums.Add(12);
+            nums.Add(19);
+            nums.Add(54);
+            nums.Add(9);
+            nums.Add(14);
+            nums.Add(67);
+            nums.Add(76);
+            nums.Add(72);
+
+        }
+    }
+}
