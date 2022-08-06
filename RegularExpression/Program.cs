4a91622aa2875d8a7b62ce50d1a8a5e7f7533a74
[prasanna] <prasanna>
2022-08-06 09:28:41 +0530

    [prasanna]: regular Expression

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex patterns = new Regex();
+            Console.WriteLine(patterns.validatePinCode("abc123"));
+            Console.WriteLine(patterns.validatePinCode("400123"));
+
+        }
+
+    }
+}
---
+++ RegularExpression/Regex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    internal class Regex
+    {
+        public static string REGEX_PINCODE = "^[1-9][0-9]{5}$";
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, REGEX_PINCODE);
+        }
+
+        
+    }
+}
