9ae08380c0c5fff4448b1ea0f078babe69b320c5
[prasanna] <prasanna>
2022-08-06 09:32:27 +0530

    [prasanna]: Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    public class Mainclass
+    {
+        private static void Main()
+        {
+            bool Equal = Calculator.AreEqual<string>("BC", "DE");
+            if (Equal)
+            {
+                Console.WriteLine("Equal");
+            }
+            else
+            {
+                Console.WriteLine("Not Equal");
+            }
+        }
+
+        public class Calculator
+        {
+            public static bool AreEqual<T>(T Value1, T Value2)
+            {
+                return Value1.Equals(Value2);
+            }
+        }
+    }
+}
+
