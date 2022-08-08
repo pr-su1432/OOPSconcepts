52bd3b562c5322f98de84a7a2080e1d7f5f8cd06
[prasanna] <prasanna>
2022-08-08 21:04:12 +0530

    [prasanna]: queues

---
+++ Queues/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        Queue countries = new Queue();
+        countries.Enqueue("India");
+        countries.Enqueue("Australia");
+        countries.Enqueue("Canada");
+        countries.Enqueue("Japan");
+        countries.Enqueue("Mexico");
+
+        foreach (string country in countries)
+        {
+            Console.WriteLine(country);
+        }
+
+        Console.WriteLine("Peek element: " + countries.Peek());
+        Console.WriteLine("Dequeue: " + countries.Dequeue());
+        Console.WriteLine("Peek element After Dequeue: " + countries.Peek());
+    }
+}
